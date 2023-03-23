c52b745ac76d97d4a58cf4f484d5081f7259bbf1
Vented5 <venteddiagram@gmail.com>
2023-03-22 21:12:43 -0600

    Respuestas json de la api funcionando

--- static/scripts/chords.js
+++ static/scripts/chords.js
@@ -9,22 +9,10 @@ const rowIndex = document.getElementById("rowIndex");
 const diapason = document.getElementById("diapason");
 var strings = [];
 
-var string1 = [12];
-for(let i=0; i<12; i++){
-    string1[i] = 0;    
-}
-
 function onRequestHandler() {
     if(this.readyState == 4 && this.status == 200){
         const data = JSON.parse(this.response);
-        
         strings = data[0].strings;
-
-        console.log(data[0]);
-
-        console.log(data[0].strings);
-        console.log(data[0].strings[0]);
-        string1[0] = data[0].strings[0];
     }
 }
 
@@ -32,10 +20,12 @@ xhr.addEventListener("load", onRequestHandler);
 xhr.open("GET", API_URL + "C");
 xhr.send();
 
+
 function getChord(chordName){
     xhr.addEventListener("load", onRequestHandler);
     xhr.open("GET", API_URL + chordName);
     xhr.send();
+    showChord();
 }
 
 
@@ -44,25 +34,36 @@ function getChord(chordName){
 const chordsNatural = ["C", "D", "E", "F", "G", "A", "B"];
 const stringsAir = ["E", "B", "G", "D", "A", "E"];
 
-//TEST FUNCTION
-    function wenasNoches(value) {
-            console.log("Chord: " + value);
-        }
-
-
 //CHORD SELECT BUTTONS
 for(let i=0; i<6; i++){
     chordButtons.innerHTML += '<button id="chordButton'+ chordsNatural[i] +'" value="'+ chordsNatural[i] +'" class="bg-sky-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onclick="getChord(this.value)">'+ chordsNatural[i] +'</button>'
 }
 
-//CORD INDEX
+//TAB INDEX
 for(let i=1; i < 13; i++){
     colIndex.innerHTML += "<span class='w-4'>" + i + "</span>";
 }
 
-//ROW INDEX
-for(let i=1; i<=6; i++){
+//STRING INDEX
+for(let i=6; i>=1; i--){
     rowIndex.innerHTML += "<h1>"+ i +"</h1>";
+}
+
+//DIAPASON
+function showChord(){
+    console.log(strings);
+    diapason.innerHTML = "";
+    for(let i=10; i>=0; i-=2){
+        console.log("La cuerda " + strings[i]);
+        if (strings[i] == "X") {
+            console.log("Coincide");
+            diapason.innerHTML += "<div class='col-span-12 w-full text-center border-r-4 border-b-2 border-white'>"+ strings[i] +"</div>"
+        }else if(strings[i] == "0"){
+            diapason.innerHTML += "<div class='col-span-12 w-full text-center border-r-4 border-b-2 border-white'></div>"
+        }else {
+            diapason.innerHTML += "<div class='col-start-"+ strings[i] +" w-full text-center border-r-4 border-b-2 border-white'>"+ strings[i] +"</div>"
+        }
+    }
 }
 
 /*//DIAPASON
